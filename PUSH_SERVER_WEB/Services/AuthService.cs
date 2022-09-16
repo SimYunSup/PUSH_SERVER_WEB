5a50132e2e2691f0b177a564b22ffcb9bcbb2c10
SimYunSup <pedogunu@gmail.com>
2022-09-16 09:56:47 +0000

    AppRouteView 수정

--- PUSH_SERVER_WEB/Services/AuthService.cs
+++ PUSH_SERVER_WEB/Services/AuthService.cs
@@ -29,11 +29,6 @@ namespace PUSH_SERVER_WEB.Services
             _httpService = httpService;
             _navigationManager = navigationManager;
             _localStorageService = localStorageService;
-            var path = new Uri(navigationManager.Uri).PathAndQuery;
-            if (path != "/login" && accessToken == null)
-            {
-                this.Refresh();
-            }
         }
 
         public async Task Login(string username, string password)
@@ -41,7 +36,7 @@ namespace PUSH_SERVER_WEB.Services
             var User = await _httpService.Post<User>("/api/user/login", new { username, password });
             await _localStorageService.SetItem("refreshToken", User?.refresh_token);
             await _localStorageService.SetItem("accessToken", User?.access_token);
-            accessToken = User?.access_token;
+
         }
 
         public async Task<User?> Refresh() {
@@ -51,7 +46,6 @@ namespace PUSH_SERVER_WEB.Services
 
             await _localStorageService.SetItem("refreshToken", Response?.refresh_token);
             await _localStorageService.SetItem("accessToken", Response?.access_token);
-            accessToken = Response?.access_token;
             return Response;
         }
         public async Task Logout()
